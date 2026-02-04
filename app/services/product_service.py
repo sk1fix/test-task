c1e6ac3b4079c0c16075d9c0e785e64407ea6645
sk1fix <agafonovtoha427@gmail.com>
2026-02-05 01:18:45 +0400

    feat: services

---
+++ app/services/product_service.py
@@ -0,0 +1,27 @@
+from schemas.product import CreateProductDto, GetProductDto
+
+
+class ProductService:
+    def __init__(self, repo):
+        self.repo = repo
+
+    async def get_by_grade(self, data):
+        result = await self.repo.get_product_by_grade(data)
+        new_dto = GetProductDto(
+            alloy_grade=result.alloy_grade,
+            weight=result.weight,
+            batch_number=result.batch_number,
+            status=result.status.value,
+            test_id=result.test_id,
+            created_at=result.created_at
+        )
+        return new_dto
+
+    async def create_product(self, data):
+        result = await self.repo.create_product(data)
+        new_dto = CreateProductDto(
+            alloy_grade=result.alloy_grade,
+            weight=result.weight,
+            batch_number=result.batch_number
+        )
+        return new_dto
---
+++ app/services/quality_service.py
@@ -0,0 +1,27 @@
+from schemas.quality_test import CreateTestDto, GetTestDto
+
+
+class QualityService:
+    def __init__(self, repo):
+        self.repo = repo
+
+    async def get_by_number(self, data):
+        result = await self.repo.get_test_by_batch_number(data)
+        new_dto = GetTestDto(
+            inspector_fullname=result.inspector_fullname,
+            standart=result.standart,
+            chemical_composition=result.chemical_composition,
+            analysis_result=result.analysis_result,
+            created_at=result.created_at
+        )
+        return new_dto
+
+    async def create_test(self, data):
+        result = await self.repo.create_test(data)
+        new_dto = CreateTestDto(
+            inspector_fullname=result.inspector_fullname,
+            standart=result.standart,
+            chemical_composition=result.chemical_composition,
+            analysis_result=result.analysis_result
+        )
+        return new_dto

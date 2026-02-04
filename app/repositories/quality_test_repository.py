40bc040cc2fb67dcc471713476b0b135673368cf
sk1fix <agafonovtoha427@gmail.com>
2026-02-05 00:29:21 +0400

    feat: quality test repository

---
+++ app/repositories/quality_test_repository.py
@@ -0,0 +1,28 @@
+from sqlalchemy import select, update
+
+from models.models import QualityTest, Product
+
+
+class QualityRepository:
+    def __init__(self, session):
+        self.session = session
+
+    async def create_test(self, data, product_batch_number):
+        async with self.session.begin():
+            query = QualityTest(**data.model_dump())
+            self.session.add(query)
+            await self.session.flush()
+
+            stmt = update(Product).where(
+                Product.batch_number == product_batch_number
+            ).values(test_id=query.id)
+            await self.session.execute(stmt)
+
+        return query
+
+    async def get_test_by_batch_number(self, batch_number):
+        query = select(QualityTest).join(Product, Product.test_id ==
+                                         QualityTest.id).where(Product.batch_number == batch_number)
+        result = await self.session.execute(query)
+
+        return result.scalars().first()
